7d6530c071b53e155fc3fbaf6bda4d50e7a62d8d
pqsk <pabloarista@icloud.com>
2015-11-07 00:22:07 -0500

    Initial commit

---
+++ Data/PantherParking.Data/DAL/Repositories/PantherParkingRepository.cs
@@ -0,0 +1,9 @@
+﻿using PantherParking.Data.DAL.Interfaces;
+
+namespace PantherParking.Data.DAL.Repositories
+{
+    public class PantherParkingRepository : IPantherParkingRepository
+    {
+         
+    }
+}
