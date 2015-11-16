0555f4be651e1d8fcfc265f39249e393f24731da
pqsk <pabloarista@icloud.com>
2015-11-15 23:29:30 -0500

    Removed HttpContentType enumeration as it is not needed, we just need a json content type for all calls to parse
    Update httpmethod enumeration
    
    Updated BaseRepository code for calling parse and created a test to see if the call would succeed. now pending hooking up the other methods

--- Data/PantherParking.Data/DAL/Repositories/BaseRepository.cs
+++ Data/PantherParking.Data/DAL/Repositories/BaseRepository.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
 using System.IO;
 using System.Net;
 using System.Net.Security;
+using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using System.Xml.Linq;
 using PantherParking.Data.Models.enumerations;
+using System.Web.Script.Serialization;
 //using Pqsk.Helper;
 //using Pqsk.Helper.Xml;
 
@@ -14,6 +17,13 @@ namespace PantherParking.Data.DAL.Repositories
 {
     public class BaseRepository
     {
+        public static void Main()
+        {
+            BaseRepository br = new BaseRepository();
+            dynamic response = br.GetRestApiResponse(BaseRepository.ParseUrlPrefix + "classes/TestObject", HttpMethod.Post, null,
+                new {foo = "bar"});
+        }
+
 #warning TODO: here we need the call to our datastore that shall be inherited by all repository classes
 
         /*
@@ -23,13 +33,38 @@ namespace PantherParking.Data.DAL.Repositories
         AvailableSpaces
         */
 
-        private string AppID => ConfigurationManager.AppSettings["appID"];
-        private string JavaScriptID => ConfigurationManager.AppSettings["javaScriptID"];
+        private static string AppID => ConfigurationManager.AppSettings["appID"];
+        private static string RestApiKey => ConfigurationManager.AppSettings["restApiKey"];
+        private static string DefaultParseHttpContentType => ConfigurationManager.AppSettings["defaultParseHttpContentType"];
 
+        private static string ParseUrlPrefix => $"{ConfigurationManager.AppSettings["parseRestApi"]}/{ConfigurationManager.AppSettings["parseVersion"]}/";
+
+        public static string GetEnumDescription(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value + "");
+
+            DescriptionAttribute[] attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(
+                typeof(DescriptionAttribute),
+                false);
+
+            return attributes.Length > 0
+                ? attributes[0].Description
+                : value.ToString();
+        }
+
+        public static string SerializeModeltoJson(object o)
+        {
+            string json = new JavaScriptSerializer().Serialize(o);
+
+            return json;
+        }
+
+#warning TODO:check if we need any headers besides the application id and the rest api key, because we don't need to use the whole parse API
         public dynamic GetRestApiResponse(string url
                                 , HttpMethod httpMethod
                                 , Dictionary<string, string> headers
-                                , HttpContentType httpContentType)
+                                , object contents)
         {
             HttpWebRequest request = null;
             HttpWebResponse response = null;
@@ -41,7 +76,7 @@ namespace PantherParking.Data.DAL.Repositories
 
                 request = (HttpWebRequest)WebRequest.Create(url);
 
-                request.Method = httpMethod + "";
+                request.Method = BaseRepository.GetEnumDescription(httpMethod);
 
                 if (headers != null && headers.Count > 0)
                 {
@@ -51,32 +86,35 @@ namespace PantherParking.Data.DAL.Repositories
                     }//foreach kvp
                 }//if
 
-                switch (httpContentType)
+                #region headers
+                request.Headers.Add("X-Parse-Application-Id", BaseRepository.AppID);
+                request.Headers.Add("X-Parse-REST-API-Key", BaseRepository.RestApiKey);
+                #endregion headers
+
+                request.ContentType = BaseRepository.DefaultParseHttpContentType;
+
+                using (StreamWriter writer = new StreamWriter(request.GetRequestStream()))
                 {
-                    case HttpContentType.NONE:
-                        request.ContentType = "";
-                        break;
-                    case HttpContentType.APPLICATION_XML:
-                        request.ContentType = "application/xml";
-                        break;
-                    case HttpContentType.APPLICATION_X_WWW_FORM_URLENCODED:
-                        request.ContentType = "application/x-www-form-urlencoded";
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(httpContentType), httpContentType, null);
-                }
 
-                //StreamWriter writer = new StreamWriter(request.GetRequestStream());
-
-                //writer.WriteLine(rawXml.ToString());
-                //writer.Close();
+                    writer.WriteLine(BaseRepository.SerializeModeltoJson(contents));
+                    writer.Flush();
+                    writer.Close();
+                }//using writer
                 // Send the data to the webserver
                 response = (HttpWebResponse)request.GetResponse();
+                string parseResponse = null;
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    parseResponse = streamReader.ReadToEnd();
+                }
+
+                return parseResponse;
+
             }
             catch (Exception ex)
             {
                 ex.Data["URL"] = url;
-                ex.Data["Content Type"] = httpContentType + "";
+                ex.Data["Content Type"] = BaseRepository.DefaultParseHttpContentType;
                 ex.Data["HTTP Method"] = httpMethod;
                 //ex.Data["HTTP Headers"] = headers.ToString(seperator: "],[", isItemWrapped: true, iWrapItems: 1);
                 throw;
--- Data/PantherParking.Data/Models/enumerations/HttpMethod.cs
+++ Data/PantherParking.Data/Models/enumerations/HttpMethod.cs
@@ -4,13 +4,15 @@ namespace PantherParking.Data.Models.enumerations
 {
     public enum HttpMethod
     {
-        [Description("HTTP POST")]
-        POST = 1,
-        [Description("HTTP GET")]
-        GET = 2,
-        [Description("HTTP DELETE")]
-        DELETE = 4,
-        [Description("HTTP PATCH")]
-        PATCH = 8,
+        [Description("POST")]
+        Post = 1,
+        [Description("GET")]
+        Get = 2,
+        [Description("DELETE")]
+        Delete = 4,
+        [Description("PATCH")]
+        Patch = 8,
+        [Description("PUT")]
+        Put = 16,
     }
 }
