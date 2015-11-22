2c378d6d36d319c1e6fb924e09bf747db05ac81f
rvian003 <rvian003@fiu.edu>
2015-11-22 14:41:57 -0500

    Registration finished, new class for responses created

--- Data/PantherParking.Data/DAL/Repositories/RegistrationRepository.cs
+++ Data/PantherParking.Data/DAL/Repositories/RegistrationRepository.cs
@@ -1,31 +1,88 @@
 ﻿using PantherParking.Data.DAL.Interfaces;
+using PantherParking.Data.Models;
+using PantherParking.Data.Models.ResponseModels;
 
 namespace PantherParking.Data.DAL.Repositories
 {
     public class RegistrationRepository : BaseRepository, IRegistrationRepository
     {
-        public bool CheckDuplicateRegistration(string email, string username)
+        public bool CheckDuplicateRegistration(string email)
         {
-#warning check in data store if email or username is being used
-            
-            return false;
+            try{
+                var duplicated = await ParseUser.Query
+                            .WhereEqualTo("Email", email)
+                            .FindAsync();
+                return duplicated;
+            }
+            catch (Exception e)
+            {
+                return e;
+            }
         }
 
-        public bool Register(string email, string username, string password, string passwordConfirm)
+        public RegistrationResponse Register(string email, string username, string password, string passwordConfirm)
         {
-#warning register user. ensure passwords match
-            var user = new ParseUser()
-            {
-                Username = "my name",
-                Password = "my pass",
-                Email = "email@example.com"
-            };
-        
-            // other fields can be set just like with ParseObject
-            user["phone"] = "415-392-0202";
-        
-            await user.SignUpAsync();
-            return true;
+           try{
+               RegistrationResponse response = new RegistrationResponse();
+                var user = new ParseUser()
+                {
+                    Username = username,
+                    Password = password,
+                    Email = email, 
+                };
+                // Adding other fields
+                user["AdminUser"] = false;
+                user["Locked"] = false;
+                user["LastLoginDateTime"] = new DateTime();
+                user["LastFailedLoginDateTime"] = null;        
+                user["NumberOfFailedLogins"] = 0;
+                user["Token"] = null;
+                user["tempPassword"] = passwordConfirm;
+                            
+                response = validateUserRegistration();
+                
+                if(response.ResponseValue)
+                {
+                    await user.SignUpAsync();
+                    return response;
+                }
+                else
+                {
+                    return response;
+                }
+           }
+           catch (Exception e)
+           {
+                throw;
+           }
         }
+        
+        public RegistrationResponse validateUserRegistration (ParseUser user)
+        {
+            try{
+                RegistrationResponse response = new RegistrationResponse();
+                bool isNewUser = CheckDuplicateRegistration(user.email); 
+                bool passwordIsMatched = user.Password.Equals(user.tempPassword);
+                user.tempPassword = null;
+                if(isNewUser == passwordIsMatched){
+                    response.ResponseValue = true;
+                    response.ResponseMessage = "SUCESS: User has been validated";
+                }
+                else
+                {
+                    response.ResponseValue = true;
+                    if(!isNewUser)
+                        response.ResponseMessage = "ERROR: User has been taken";
+                    else
+                        response.ResponseMessage = "ERROR: Passwords don't match";
+                }
+                    
+                return response;
+            }
+            catch (Exception e)
+            {
+                    throw;
+            }
+        }  
     }
 }
