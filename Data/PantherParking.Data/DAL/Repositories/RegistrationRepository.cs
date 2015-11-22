07e0ce94cba2aa105806475dc406942e31d2da6d
rvian003 <rvian003@fiu.edu>
2015-11-22 12:11:14 -0500

    Rodolfo attemtp to push

--- Data/PantherParking.Data/DAL/Repositories/RegistrationRepository.cs
+++ Data/PantherParking.Data/DAL/Repositories/RegistrationRepository.cs
@@ -7,12 +7,24 @@ namespace PantherParking.Data.DAL.Repositories
         public bool CheckDuplicateRegistration(string email, string username)
         {
 #warning check in data store if email or username is being used
+            
             return false;
         }
 
         public bool Register(string email, string username, string password, string passwordConfirm)
         {
 #warning register user. ensure passwords match
+            var user = new ParseUser()
+            {
+                Username = "my name",
+                Password = "my pass",
+                Email = "email@example.com"
+            };
+        
+            // other fields can be set just like with ParseObject
+            user["phone"] = "415-392-0202";
+        
+            await user.SignUpAsync();
             return true;
         }
     }
