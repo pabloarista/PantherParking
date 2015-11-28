df77ee1ff1ad4e584a419b8c59a60932e5b9822c
pqsk <pabloarista@icloud.com>
2015-11-27 23:22:06 -0500

    Problems with merge, readded over-written code (needs to be fixed)

--- Data/PantherParking.Data/DAL/Repositories/LocationRepository.cs
+++ Data/PantherParking.Data/DAL/Repositories/LocationRepository.cs
@@ -1,4 +1,9 @@
-﻿using PantherParking.Data.DAL.Interfaces;
+﻿using System.Collections.Generic;
+using System.Net;
+using PantherParking.Data.DAL.Interfaces;
+using PantherParking.Data.Models;
+using PantherParking.Data.Models.Parse;
+using PantherParking.Data.Models.ResponseModels;
 
 namespace PantherParking.Data.DAL.Repositories
 {
@@ -14,6 +19,42 @@ namespace PantherParking.Data.DAL.Repositories
         {
             //Pablo this was here before, I dodnt want to delete it in case you actually put it on purpose
             throw new System.NotImplementedException();
+        }
+
+        public bool CheckIn(string username, string garageID, string token)
+        {
+            ResponseParse<User> r = base.GetObject<User>(token, new Dictionary<string, string>(1) { { "userName", username } });
+
+            if (r == null || r.HttpStatusCode != HttpStatusCode.OK || r.ResponseBody == null)
+            {
+                return false;
+            }//if
+
+            User u = r.ResponseBody;
+            u.garageID = garageID;
+
+            ResponseParse<ObjectUpdatedResponse> updateResponse = base.UpdateObject(u, token);
+
+            return updateResponse?.ResponseBody != null;
+        }
+
+        public bool CheckOut(string username, string token)
+        {
+            ResponseParse<User> r = base.GetObject<User>(token, new Dictionary<string, string>(1) { { "userName", username } });
+
+            if (r == null || r.HttpStatusCode != HttpStatusCode.OK || r.ResponseBody == null)
+            {
+                return false;
+            }//if
+
+            User u = r.ResponseBody;
+            u.garageID = null;
+
+            ResponseParse<ObjectUpdatedResponse> updateResponse = base.UpdateObject(u, token);
+
+            return updateResponse?.ResponseBody != null;
+
+
         }
     }
 }
